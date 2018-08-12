cd4bca97472aae6d96bbb34e051d6cc1c7240788
Sheldon Zamora-Soon <sheldonzs@gmail.com>
2018-08-11 22:14:42 -0400

    Starting glitch code and animations

---
+++ Assets/Glitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glitch : MonoBehaviour {
+
+    private int HP;
+
+    private GlitchType type;
+    private Vector2 location;
+    private float movementSpeed;
+
+    private GlitchState state;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public enum GlitchType { dasher, scanner, looper, eratic, mom}
+public enum GlitchState { moving, action, dying}
